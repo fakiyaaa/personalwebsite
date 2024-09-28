d645835570452fb07e53fc2495e8b7253049fd12
fakiyaaa <149144145+fakiyaaa@users.noreply.github.com>
2024-09-28 11:19:50 +0800

    Initial commit

---
+++ learningreact.js
@@ -0,0 +1,17 @@
+var modal = document.getElementById("cvModal");
+var btn = document.getElementById("cvButton");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
