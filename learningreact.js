4791918fb84b2fcdbf75c394d67202050f4b2df6
fakiyaaa <149144145+fakiyaaa@users.noreply.github.com>
2024-10-02 21:19:42 +0800

    Update HTML, CSS, and JavaScript files with new content and style adjustments

--- learningreact.js
+++ learningreact.js
@@ -1,18 +1,22 @@
-var modal = document.getElementById("cvModal");
-var btn = document.getElementById("cvButton");
-var span = document.getElementsByClassName("close")[0];
+document.addEventListener("DOMContentLoaded", function() {
+  var modal = document.getElementById("cvModal");
+  var btn = document.getElementById("cvButton");
+  var span = document.getElementsByClassName("close")[0];
 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+  // Open the modal when the button is clicked
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
 
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-  if (event.target == modal) {
+  // Close the modal when the 'X' (span) is clicked
+  span.onclick = function() {
     modal.style.display = "none";
   }
-}
 
+  // Close the modal when clicking outside the modal content
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+});
